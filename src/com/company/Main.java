7557b26231c92ad9bb7da82349f478403361bc65
Sachman19 <sachindmishra@knights.ucf.edu>
2021-06-06 01:34:53 -0400

    Version 1 Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Sachin Mishra
+ */
+
 package com.company;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter a noun. ");
+        String noun = input.nextLine();
+
+        System.out.print("Please enter a verb. ");
+        String verb = input.nextLine();
+
+        System.out.print("Please enter a adjective. ");
+        String adjective = input.nextLine();
+
+        System.out.print("Please enter a adverb. ");
+        String adverb = input.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s? That's Hilarious." , verb, adjective, noun, adverb);
     }
 }
